b8667f504aacda926d69140eef470eb5b23c6ee9
ShCalderon <secalderon222@umariana.edu.co>
2026-09-17 11:24:32 -0500

    Actualizacion valores

--- frontend-web/dashboard/src/services/api.js
+++ frontend-web/dashboard/src/services/api.js
@@ -6,7 +6,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
 });
 
 // Agrega el token JWT en cada request si existe
